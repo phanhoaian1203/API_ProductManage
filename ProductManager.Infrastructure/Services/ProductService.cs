4843072c8faf88ed92a897ba6909b58a74efb8fd
phanhoaian1203 <phanhoaian10b3@gmail.com>
2026-01-04 12:26:17 +0700

    fix bugs

--- ProductManager.Infrastructure/Services/ProductService.cs
+++ ProductManager.Infrastructure/Services/ProductService.cs
@@ -12,56 +12,56 @@ namespace ProductManager.Infrastructure.Sevices
 {
     public class ProductService : IProductService
     {
-        private readonly IProductRepository _repository;
-        private readonly ICategoryRepository _repositoryCate;
-        public ProductService(IProductRepository repository, ICategoryRepository repository1)
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public ProductService(IProductRepository productRepository, ICategoryRepository categoryRepository)
         {
-            _repository = repository;
-            _repositoryCate = repository1;
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
         }
         public async Task<Product> AddProductAsync(Product product)
         {
-            var category = await _repositoryCate.GetByIdAsync(product.CategoryId);
+            var category = await _categoryRepository.GetByIdAsync(product.CategoryId);
             if (category == null)
             {
                 // Ném lỗi 404 để Middleware bắt
                 throw new KeyNotFoundException($"Danh mục có ID {product.CategoryId} không tồn tại!");
             }
-            return await _repository.AddAsync(product);
+            return await _productRepository.AddAsync(product);
         }
 
         public async Task DeleteProductAsync(int id)
         {
-            var proDel = await _repository.GetByIdAsync(id);
+            var proDel = await _productRepository.GetByIdAsync(id);
             if (proDel == null) throw new KeyNotFoundException("Không tìm thấy sản phẩm để xóa!!!");
-            await _repository.DeleteAsync(id);
+            await _productRepository.DeleteAsync(id);
         }
 
         public async Task<List<Product>> GetAllProductsAsync()
         {
-            var products = await _repository.GetAllAsync();
-            if (products.Count == 0) throw new KeyNotFoundException("Không tìm thấy sản phẩm nào cả!!!");
+            var products = await _productRepository.GetAllAsync();
+            
             return products;
         }
 
         public async Task<Product> GetProductByIdAsync(int id)
         {
-            var product = await _repository.GetByIdAsync(id);
+            var product = await _productRepository.GetByIdAsync(id);
             if (product == null) throw new KeyNotFoundException("Không tìm thấy sản phẩm !!!");
             return product;
         }
 
         public async Task UpdateProductAsync(int id, Product product)
         {
-            var proUp = await _repository.GetByIdAsync(id);
+            var proUp = await _productRepository.GetByIdAsync(id);
             if (proUp == null) throw new KeyNotFoundException("Không tìm thấy sản phẩm để cập nhật!!!");
             proUp.Name = product.Name;
             proUp.Price = product.Price;
             proUp.Stock = product.Stock;
-            var pro = await _repositoryCate.GetByIdAsync(id);
+            var pro = await _categoryRepository.GetByIdAsync(product.CategoryId);
             if (pro == null) throw new KeyNotFoundException("Không tìm thấy danh mục để cập nhật!!!");
             proUp.CategoryId = product.CategoryId;
-            await _repository.UpdateAsync(proUp);
+            await _productRepository.UpdateAsync(proUp);
         }
     }
 }
